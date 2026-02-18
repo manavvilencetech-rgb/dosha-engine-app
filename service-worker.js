1b28bca7da4bc4998843810d42090a4768c7a149
manavvilencetech-rgb <manav.vilencetech@gmail.com>
2026-02-18 14:55:20 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,39 @@
 const CACHE_NAME = 'dosha-cache-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './logo.png',
-  './manifest.json'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo.png',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-// Install
+// Install service worker and cache files
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-// Activate
+// Activate service worker
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.map(key => {
-        if(key !== CACHE_NAME) return caches.delete(key);
+        if (key !== CACHE_NAME) return caches.delete(key);
       }))
     )
   );
+  self.clients.claim();
 });
 
-// Fetch
+// Fetch cached files if offline
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
 
